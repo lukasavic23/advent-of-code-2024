0bc2a1fcf7d8feb91a79c8276bfb210b093390e3
Luka Savic <77903056+lukasavic23@users.noreply.github.com>
2024-12-01 23:36:50 +0100

    day one part two

--- day1/day1.js
+++ day1/day1.js
@@ -1,6 +1,7 @@
 import { leftList } from "./List1.js";
 import { rightList } from "./List2.js";
 
+// Part One
 function sortBySmallestNum(array) {
   return array.sort((a, b) => a - b);
 }
@@ -9,8 +10,19 @@ const sortright = sortBySmallestNum(rightList);
 
 let completeDistance = 0;
 for (let i = 0; i < sortleft.length; i++) {
-  const newEntry = [sortleft[i], sortright[i]];
-
   const distance = Math.abs(sortright[i] - sortleft[i]);
   completeDistance = completeDistance + distance;
 }
+
+console.log(completeDistance, "part one result");
+
+// Part Two
+let repetition = 0;
+for (const l of leftList) {
+  const timesShowedUp = rightList.filter((r) => r === l).length;
+
+  const newNum = l * timesShowedUp;
+  repetition += newNum;
+}
+
+console.log(repetition, "part two result");
