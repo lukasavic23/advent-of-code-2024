d259ee71521e0627eef7f158206868f09041a3bc
Luka Savic <77903056+lukasavic23@users.noreply.github.com>
2024-12-05 00:17:20 +0100

    day 3 part two

--- day3/index.js
+++ day3/index.js
@@ -12,3 +12,25 @@ match.forEach((mt) => {
 });
 
 console.log(sum, "part one result");
+const matchedOccurencies = inputData.match(/(mul)\([0-9]+,[0-9]+\)|do(|n't)\(\)/g);
+
+const muls = [];
+let isPushing = true;
+
+for (const mt of matchedOccurencies) {
+  if (mt === "don't()") {
+    isPushing = false;
+  } else if (mt === "do()") {
+    isPushing = true;
+  } else if (isPushing) {
+    muls.push(mt);
+  }
+}
+let sum2 = 0;
+muls.forEach((mt) => {
+  const [first, second] = mt.match(/[0-9]+/g);
+  const result = +first * +second;
+  sum2 += result;
+});
+
+console.log(sum2, "part two result");
