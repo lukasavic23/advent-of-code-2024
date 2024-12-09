a122bcea36840456489c946e81ac1e0282354475
Luka Savic <77903056+lukasavic23@users.noreply.github.com>
2024-12-09 20:51:42 +0100

    day 5 part two

--- day5/index.js
+++ day5/index.js
@@ -18,11 +18,7 @@ function isCorrectOrder(pageUpdates) {
     const second = order[1];
     const indxF = pageUpdates.indexOf(first);
     const indxS = pageUpdates.indexOf(second);
-    if (
-      indxF > -1 &&
-      indxS > -1 &&
-      (indxF > indxS)
-    ) {
+    if (indxF > -1 && indxS > -1 && indxF > indxS) {
       return [false, 0];
     }
   }
@@ -38,3 +34,42 @@ for (let i = 0; i < data.length; i++) {
   }
 }
 console.log(total);
+
+function sortCorrectly(pageUpdates) {
+  for (const order of ordering) {
+    const first = order[0];
+    const second = order[1];
+    const indxF = pageUpdates.indexOf(first);
+    const indxS = pageUpdates.indexOf(second);
+    if (indxF > -1 && indxS > -1 && indxF > indxS) {
+      const temp = pageUpdates[indxF];
+      pageUpdates[indxF] = pageUpdates[indxS];
+      pageUpdates[indxS] = temp;
+    }
+  }
+  return pageUpdates;
+}
+
+const notCorrect = data.filter((arr) => !isCorrectOrder(arr)[0]);
+const corrected = [];
+while (notCorrect.length > 0) {
+  for (let i = 0; i < notCorrect.length; i++) {
+    const arr = notCorrect[i];
+    const newArr = sortCorrectly(arr);
+    const [isCorrect] = isCorrectOrder(newArr);
+    if (isCorrect) {
+      notCorrect.splice(i, 1);
+      corrected.push(newArr);
+    }
+  }
+}
+
+let total2 = 0;
+for (let i = 0; i < corrected.length; i++) {
+  const arr = corrected[i];
+  const [isCorrect, middleNum] = isCorrectOrder(arr);
+  if (isCorrect) {
+    total2 += middleNum;
+  }
+}
+console.log(total2);
