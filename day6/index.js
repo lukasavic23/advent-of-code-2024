5fb24efe3ab1d6d2227caaa4a0e169806dfcbc9d
Luka Savic <77903056+lukasavic23@users.noreply.github.com>
2024-12-12 20:30:17 +0100

    day 6 part two

--- day6/index.js
+++ day6/index.js
@@ -36,9 +36,12 @@ directionMap.set(DOWN, [1, 0]);
 directionMap.set(RIGHT, [0, 1]);
 directionMap.set(LEFT, [0, -1]);
 
+let rowI2 = rowI;
+let posI2 = posI;
+
 const visitedPositions = new Set();
 while (true) {
-  visitedPositions.add(`row index: ${rowI}, position index: ${posI}`);
+  visitedPositions.add(`${rowI},${posI}`);
 
   const nextRow = rowI + directionMap.get(currentDirection)[0];
   const nextPosition = posI + directionMap.get(currentDirection)[1];
@@ -56,3 +59,55 @@ while (true) {
   }
 }
 console.log(visitedPositions.size, "visited positions");
+
+const inputData2 = inputData.map((row) =>
+  row.split(",").flatMap((row) => row.split(""))
+);
+
+function willGuardLoop(newRowI, newPosI) {
+  if (inputData2[newRowI][newPosI] === "#") {
+    return false;
+  }
+  inputData2[newRowI][newPosI] = "#";
+
+  let currentDirection2 = UP;
+  const visitedPositions2 = new Set();
+
+  let tempRowI = rowI2;
+  let tempPosI = posI2;
+
+  while (true) {
+    if (visitedPositions2.has(`${tempRowI}-${tempPosI}-${currentDirection2}`)) {
+      inputData2[newRowI][newPosI] = ".";
+      return true;
+    }
+    visitedPositions2.add(`${tempRowI}-${tempPosI}-${currentDirection2}`);
+
+    const nextRow = tempRowI + directionMap.get(currentDirection2)[0];
+    const nextPosition = tempPosI + directionMap.get(currentDirection2)[1];
+
+    const isRowInBound = nextRow >= 0 && nextRow < inputData.length;
+    const isPositionInBound =
+      nextPosition >= 0 && nextPosition < inputData[0].length;
+    if (!(isRowInBound && isPositionInBound)) {
+      inputData2[newRowI][newPosI] = ".";
+      return false;
+    } else if (inputData2[nextRow][nextPosition] === "#") {
+      currentDirection2 = nextDirection.get(currentDirection2);
+    } else {
+      tempRowI = nextRow;
+      tempPosI = nextPosition;
+    }
+  }
+}
+
+let loops = 0;
+visitedPositions.forEach((position) => {
+  const [i, j] = position.split(",");
+  if (i == rowI2 && j == posI2) return;
+  const willLoop = willGuardLoop(i, j);
+  if (willLoop) {
+    loops++;
+  }
+});
+console.log(loops, "possible loops");
